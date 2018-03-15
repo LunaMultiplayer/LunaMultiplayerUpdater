aa73fc834afb33cf16971d470770cecb63c24791
4669842 <32111986+4669842@users.noreply.github.com>
2018-03-15 12:45:50 +0000

    Luna Manager

---
+++ Manager/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Manager
+{
+
+    class MainMenu
+    {
+        static void Main()
+
+        {
+
+            kerbalCheck();
+
+            Menu();
+
+        }
+
+        private static void kerbalCheck()
+        {
+            Process[] kerbalProcess = Process.GetProcessesByName("KSP");
+            Process[] kerbalProcess64 = Process.GetProcessesByName("KSP_x64");
+            if (kerbalProcess.Length > 0)
+            {
+                Console.WriteLine("Kerbal Space Program is already Running ");
+            }
+            if (kerbalProcess64.Length > 0)
+            {
+                Console.WriteLine("Kerbal Space Program is already Running ");
+            }
+            else
+            {
+                Console.WriteLine("Kerbal Space Program is not running.");
+            }
+
+        }
+        private static void Menu()
+        {
+            var menu = new EasyConsole.Menu()
+   .Add("Run Kerbal Space Program", () => Console.WriteLine("Booting Kerbal Space Program"))
+   .Add("Install/Update LunaMultiplayer", () => Console.WriteLine("Luna Selected"));
+            menu.Display();
+        }
+    }
+}
