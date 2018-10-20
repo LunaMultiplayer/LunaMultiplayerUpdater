f1ebd620d0d17d5b67e51e60a5810d420535797b
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-10-20 21:26:20 +0200

    Update for KSP 1.5

--- ClientUpdater/Program.cs
+++ ClientUpdater/Program.cs
@@ -11,8 +11,7 @@ namespace ClientUpdater
         {
             Console.WriteLine("This program will download the latest UNSTABLE version and replace your current LMPClient");
 
-            if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "KSP.exe")) && 
-                !File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "KSP_x64.exe")))
+            if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "KSP_x64.exe")))
                 Console.WriteLine("Please drop \"Client LMP Updater\" in the main KSP folder next to KSP.exe/KSP_x64.exe!");
             else
             {
