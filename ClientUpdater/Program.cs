cea848cd35c7d2ad3aea556c503bfb6233ecd803
Gabriel Vázquez (Dagger) <gavazquez@gmail.com>
2018-11-29 18:01:52 +0100

    Fix file check

--- ClientUpdater/Program.cs
+++ ClientUpdater/Program.cs
@@ -11,7 +11,7 @@ namespace ClientUpdater
         {
             Console.WriteLine("This program will download the latest UNSTABLE version and replace your current LMPClient");
 
-            if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "KSP_x64.exe")) || !File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "KSP.x86_64")))
+            if (!File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "KSP_x64.exe")) && !File.Exists(Path.Combine(Directory.GetCurrentDirectory(), "KSP.x86_64")))
                 Console.WriteLine("Please drop \"Client LMP Updater\" in the main KSP folder next to KSP.x86_64/KSP_x64.exe!");
             else
             {
